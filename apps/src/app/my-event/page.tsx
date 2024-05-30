514e566cca8194056a9ff71152275a2c05833a9d
Chea_Pinglong <cheapinglong312@gmail.com>
2024-05-31 01:07:07 +0700

    fix margin

--- apps/src/app/my-event/page.tsx
+++ apps/src/app/my-event/page.tsx
@@ -5,14 +5,14 @@ import React from "react";
 const page = () => {
   return (
     <div className="w-full h-full bg-[#FAFAFA] mt-0">
-      <div className=" md:mx-[120px] mt-[95px]  bg-white">
+      <div className=" md:ml-[300px] md:mr-[286px] mt-[95px] h-full rounded-[15px] bg-white">
         <Typography
           fontSize="h3"
           fontWeight="bold"
           align="left"
-          className="px-5"
+          className="px-[17px] pt-9"
         >
-          My Event
+          My Events
         </Typography>
         <MyEventCardList />
       </div>
