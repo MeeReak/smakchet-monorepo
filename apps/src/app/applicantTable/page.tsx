a131f54212128ed929ec580778efdcbdfc4db63a
Chea_Pinglong <cheapinglong312@gmail.com>
2024-06-02 22:40:00 +0700

    fix apply table

--- apps/src/app/applicantTable/page.tsx
+++ apps/src/app/applicantTable/page.tsx
@@ -1,28 +1,29 @@
 "use client";
 import React, { useContext } from "react";
-import { Typography, Button } from "@/components";
+import { Typography, Button, ApplyTable } from "@/components";
 import { MyContext } from "@/contexts/CardContext";
 import Image from "next/image";
 import { Table } from "@nextui-org/react";
-import Link  from "next/link";
+import Link from "next/link";
 
 const Page = () => {
   const { CardUser } = useContext(MyContext);
 
   const totalCandidate = CardUser.length;
-  const acceptedCandidate = CardUser.filter(user => user.status === "Accepted").length
+  const acceptedCandidate = CardUser.filter(
+    (user) => user.status === "Accepted"
+  ).length;
   console.log(totalCandidate);
 
   return (
- 
     <div className="bg-[#FAFAFA] w-full h-screen justify-center items-start flex">
       <div className="w-fit ml-[121px] mr-[128px]">
         {/* Header section */}
-        <div className="mt-[30px] mb-[55px] flex flex-row items-center justify-between">
+        <div className="mt-[30px] mb-[55px] flex flex-row items-center justify-between ">
           {/* Go back */}
-          <div> 
+          <div>
             <Button
-              className="!outline-none !border-none"
+              className="!outline-none !border-none md:text-2xl md:font-normal"
               leftIcon={
                 <Image
                   src={"assets/icons/back.svg"}
@@ -32,13 +33,16 @@ const Page = () => {
                 />
               }
             >
-              <Typography fontSize="h2">Applied</Typography>
+              <Link href={"../"}>
+
+              Applied
+              </Link>
             </Button>
           </div>
           {/* number of Applied & Accepted */}
           <div className="flex flex-row justify-center items-end gap-x-[13px] ">
             <Button
-              className="cursor-default justify-center items-center w-[141px] h-[50px] border-[0.4] border-[#BDBDBD] rounded-[10px]"
+              className="cursor-default justify-center items-center w-[141px] h-[50px] border-1 border-opacity-40 border-gray-400 rounded-[10px]"
               leftIcon={
                 <Image
                   src={"assets/icons/people.svg"}
@@ -48,10 +52,10 @@ const Page = () => {
                 />
               }
             >
-              <Typography fontSize="h5">{`${totalCandidate}`} Applied</Typography>
+              {`${totalCandidate}`} Applied
             </Button>
             <Button
-              className="cursor-default items-center justify-center w-[155px] h-[50px] border-[0.4] border-[#BDBDBD] rounded-[10px]"
+              className="cursor-default items-center justify-center w-[155px] h-[50px] border-1 border-opacity-40 border-gray-400 rounded-[10px]"
               leftIcon={
                 <Image
                   src={"assets/icons/check-inline.svg"}
@@ -61,57 +65,13 @@ const Page = () => {
                 />
               }
             >
-              <Typography fontSize="h5">{`${acceptedCandidate}`} Accepted</Typography>
+              {`${acceptedCandidate}`} Accepted
             </Button>
           </div>
         </div>
         {/* Table of applied volunteer */}
-        <div>
-          <table className="w-fit table-auto rounded-t-[15px] mb-20 overflow-hidden ">
-            {/* table header */}
-            <thead className="bg-[#D2E5FF] h-[60px] text-start ">
-              <tr className="text-center w-fit">
-                <th className="pl-[30px] w-[112px] ">
-                  <Typography align="left"
-                   fontSize="h4" color="grey">N°</Typography>
-                </th>
-                <th className="w-[237px]">
-                  <Typography align="left" fontSize="h4" color="grey">Name</Typography>
-                </th>
-                <th  className="w-[285px]">
-                  <Typography align="left" fontSize="h4" color="grey">Email</Typography>
-                </th>
-                <th className="w-[193px]">
-                  <Typography align="left" fontSize="h4" color="grey">Status</Typography>
-                </th>
-                <th className="w-[153px]">
-                  <Typography align="left" fontSize="h4" color="grey">Date</Typography>
-                </th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody className="text-start bg-white">
-              {CardUser.map((item, index) => (
-                <tr className="h-[65px]" key={index}>
-                  <td className="pl-[30px]">
-                   {`0${item.id}`}
-                  </td>
-                  <td>{item.name}</td>
-                  <td>{item.gmail}</td>
-                  <td className={`${item.status === "Accepted" ? "text-green-500": item.status === "Rejected"? "text-red-500": "text-gray-500"}`}>{item.status}</td>
-                  <td>{item.date}</td>
-                  <td className="pr-[57px]">
-                      <Button className="flex  items-center w-[75px] h-[35px] hover:bg-[#207BFF] !rounded-[15px] text-center justify-center bg-white hover:text-white !text-[12px] text-[#757575]">
-                        <Link href={`applicantTable/${item.id}`}>
-                        
-                        View
-                        </Link>
-                      </Button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+        <div className="mb-20">
+          <ApplyTable />
         </div>
       </div>
     </div>
