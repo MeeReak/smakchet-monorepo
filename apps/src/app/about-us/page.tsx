766bd186a89313d881a86c0a815d8765968f03af
Chea_Pinglong <cheapinglong312@gmail.com>
2024-06-04 01:04:03 +0700

    add responsive to about-us

--- apps/src/app/about-us/page.tsx
+++ apps/src/app/about-us/page.tsx
@@ -7,81 +7,272 @@ import { TeamCard, Typography } from "@/components";
 
 const Homepage = () => {
   return (
+    // <>
+    //   <div className="bg-[#FAFAFA] w-full h-full">
+    //     <div className="md:py-[126px] md:px-[180px] ">
+    //       {/* why smakchet */}
+    //       <div className="flex md:flex-row bg-white rounded-[10px] md:px-[110px] md:py-10 gap-x-[54px]">
+    //         <Image
+    //           src={"/assets/image/why_smakchet.svg"}
+    //           alt={"why_smakchet"}
+    //           width={379}
+    //           height={263}
+    //           className=""
+    //         />
+    //         <div className="flex md:flex-col w-full justify-start items-center md:my-10">
+    //           <Typography
+    //             className="md:!text-2xl !font-semibold w-full"
+    //             fontWeight="bold"
+    //             align="right"
+    //           >
+    //             Why SmakChet?
+    //           </Typography>
+    //           <hr className="w-[75px] h-[3px] bg-[#207BFF] mr-28"></hr>
+    //           <Typography className="!text-base !font-normal mt-[30px]" align="left">
+    //             SmakChet is a Website that include all your needed about
+    //             volunteers. We have process all of your detail about event, date
+    //             mange users etc.
+    //           </Typography>
+    //         </div>
+    //       </div>
+
+    //       {/* our vision our mission */}
+    //       <div className="md:mt-[46px] md:mx-[130px] md:py-[25px] md:px-5 gap-x-24 flex md:flex-row justify-center">
+    //         <div className="flex md:flex-col items-center">
+    //           <Typography fontWeight="normal" className="md:!text-2xl">
+    //             Our Vision
+    //           </Typography>
+    //           <hr className="w-[62px] h-[2px] bg-[#207BFF] justify-center"></hr>
+    //           <Typography className="md:!text-base" align="center">
+    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    //             eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+    //           </Typography>
+    //         </div>
+    //         <div className="flex md:flex-col items-center">
+    //           <Typography fontWeight="normal" className="md:!text-2xl">
+    //             Our Mission
+    //           </Typography>
+    //           <hr className="w-[62px] h-[2px] bg-[#207BFF] justify-center"></hr>
+    //           <Typography className="md:!text-base" align="center">
+    //             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+    //             eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+    //           </Typography>
+    //         </div>
+    //       </div>
+
+    //       {/* Our team */}
+    //       <div className="md:mt-[61px] bg-white rounded-[10px] flex md:py-[25px] justify-center">
+    //         <div className="flex md:flex-col align-middle items-center">
+
+    //           <Typography
+    //             className="md:!text-2xl !font-semibold w-full"
+    //             fontWeight="bold"
+    //             align="center"
+    //           >
+    //             Our Team
+    //           </Typography>
+    //           <hr className="w-[58px] h-[2px] bg-[#207BFF] mr-10"></hr>
+    //           <Typography className="md:!text-base md:mt-[28px]">
+    //             Our SmakChet members have become more responsibility on our own
+    //             role:
+    //           </Typography>
+    //           <div className="grid md:grid-cols-2 md:mt-4 md:justify-between items-center gap-x-24 align-middle w-full gap-y-4">
+    //             <TeamCard />
+    //             <TeamCard />
+    //             <TeamCard />
+    //             <TeamCard />
+    //           </div>
+    //         </div>  
+    //       </div>
+               
+    //       {/* Our partner */}
+    //       <div className="bg-white flex md:flex-col justify-center items-center md:mt-[66px] md:py-[25px] gap-y-[47px]">
+    //         <div>
+
+    //       <Typography
+    //           className="md:!text-2xl !font-semibold w-full"
+    //           fontWeight="bold"
+    //           align="center"
+    //         >
+    //           Partner
+    //       </Typography>
+    //       <hr className="w-[58px] h-[2px] bg-[#207BFF]"></hr>
+    //         </div>
+    //       <Typography className="md:!text-base md:font-normal">It&apos;s an honor to participate with our partner </Typography>
+    //       <div className="flex md:flex row w-full px-[233px] justify-between">
+    //       <Link href={"https://sabaicode.com/"}>
+    //           <Image
+    //             src={"/assets/image/sabaicode.svg"}
+    //             alt={"profile"}
+    //             width={100}
+    //             height={100}
+    //             className="object-fill"
+    //           />
+    //         </Link>
+    //         <Link href={"https://chekromlek.com/"}>
+    //           <Image
+    //             src={"/assets/image/weshare.png"}
+    //             alt={"profile"}
+    //             width={100}
+    //             height={100}
+    //           />
+    //         </Link>
+    //       <Link href={"https://neakhatka.com/"}>
+    //           <Image
+    //             src={"/assets/image/internship.svg"}
+    //             alt={"profile"}
+    //             width={100}
+    //             height={100}
+    //           />
+    //         </Link>
+    //         <Link href={"https://learnwithkru.com/"}>
+    //           <Image
+    //             src={"/assets/image/kru.png"}
+    //             alt={"profile"}
+    //             width={100}
+    //             height={100}
+    //           />
+    //         </Link>
+    //       </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </>
     <>
-      <div className="xl:w-[1024px] m-auto space-y-5 z-10 xl:mt-[100px] w-screen">
-        {/* Why SmakChet? */}
-        <div className="xl:flex mb-10">
-          <div className="xl:w-[50%] flex justify-center">
-            <Image
-              src={"/assets/image/why_smakchet.svg"}
-              alt={"why_smakchet"}
-              width={238}
-              height={151}
-              className="xl:w-[472px] xl:h-[299px]"
-            ></Image>
-          </div>
-          <div className="xl:w-[50%] mt-4">
-            <Typography fontSize="h1" fontWeight="bold" align="center">
-              Why SmakChet?
-            </Typography>
-            <hr className="w-[75px] h-[7px] bg-[#207BFF] mx-auto my-4"></hr>
-            <Typography fontSize="h3" align="center">
-              SmakChet is a Website that include all your needed about
-              volunteers. We have process all of your detail about event, date
-              mange users etc.
-            </Typography>
-          </div>
+  <div className="bg-[#FAFAFA] w-full h-full">
+    <div className="py-10 px-5 md:py-[126px] md:px-[180px]">
+      {/* why smakchet */}
+      <div className="flex flex-col md:flex-row bg-white rounded-[10px] px-5 py-10 md:px-[110px] gap-y-5 md:gap-y-0 md:gap-x-[54px]">
+        <Image
+          src={"/assets/image/why_smakchet.svg"}
+          alt={"why_smakchet"}
+          width={379}
+          height={263}
+          className="self-center md:self-start"
+        />
+        <div className="flex flex-col w-full justify-start items-center md:items-start md:my-10">
+          <Typography
+            className="!text-xl md:!text-2xl font-semibold w-full !text-center md:!text-left"
+            fontWeight="bold"
+            
+          >
+            Why SmakChet?
+          </Typography>
+          <hr className="w-[75px] h-[3px] bg-[#207BFF] my-2"></hr>
+          <Typography className="!text-base font-normal mt-[30px]" align="left">
+            SmakChet is a Website that include all your needed about
+            volunteers. We have process all of your detail about event, date
+            mange users etc.
+          </Typography>
         </div>
-        {/* Our team */}
-        <div className="w-full">
-          <Typography fontSize="h1" fontWeight="bold" align="center">
+      </div>
+
+      {/* our vision our mission */}
+      <div className="mt-10 md:mt-[46px] mx-5 md:mx-[130px] py-10 md:py-[25px] px-5 gap-y-10 gap-x-0 md:gap-x-24 hidden md:flex flex-col md:flex-row justify-center">
+        <div className="flex flex-col items-center">
+          <Typography fontWeight="normal" className="!text-xl md:!text-2xl">
+            Our Vision
+          </Typography>
+          <hr className="w-[62px] h-[2px] bg-[#207BFF] my-2"></hr>
+          <Typography className="!text-base" align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+        </div>
+        <div className="flex flex-col items-center">
+          <Typography fontWeight="normal" className="!text-xl md:!text-2xl">
+            Our Mission
+          </Typography>
+          <hr className="w-[62px] h-[2px] bg-[#207BFF] my-2"></hr>
+          <Typography className="!text-base" align="center">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+        </div>
+      </div>
+
+      {/* Our team */}
+      <div className="mt-10 md:mt-[61px] bg-white rounded-[10px] flex py-10 md:py-[25px] justify-center">
+        <div className="flex flex-col align-middle items-center">
+          <Typography
+            className="!text-xl md:!text-2xl font-semibold w-full"
+            fontWeight="bold"
+            align="center"
+          >
             Our Team
           </Typography>
-          <hr className="w-[75px] h-[7px] bg-[#207BFF] mx-auto my-4"></hr>
-          <Typography fontSize="h3" align="center">
+          <hr className="w-[58px] h-[2px] bg-[#207BFF] my-2"></hr>
+          <Typography className="!text-base mt-[28px]">
             Our SmakChet members have become more responsibility on our own
             role:
           </Typography>
-        </div>
-        {/* profile */}
-        <div className="grid xl:grid-cols-2 xl:grid-rows-2 gap-y-7 content-center">
-          <TeamCard />
-          <TeamCard />
-          <TeamCard />
-          <TeamCard />
-        </div>
-        {/* Partner */}
-        <div className="w-full">
-          <div>
-            <Typography fontSize="h1" fontWeight="bold" align="center">
-              Our Partner
-            </Typography>
-            <hr className="w-[75px] h-[7px] bg-[#207BFF] mx-auto my-4"></hr>
-            <Typography fontSize="h3" align="center">
-              It’s an honor to participate with our partner
-            </Typography>
-          </div>
-          <div className="w-full mt-10 flex flex-col items-center justify-center gap-y-3 xl:flex-row xl:justify-between">
-            <Link href={"https://sabaicode.com/"} className="mb-5 xl:mb-0">
-              <Image
-                src={"/assets/image/sabaicode.svg"}
-                alt={"profile"}
-                width={200}
-                height={200}
-              />
-            </Link>
-            <Link href={"https://neakhatka.com/"}>
-              <Image
-                src={"/assets/image/internship.svg"}
-                alt={"profile"}
-                width={200}
-                height={200}
-              />
-            </Link>
+          <div className="grid grid-cols-1 md:grid-cols-2 mt-4 justify-between items-center gap-x-24 align-middle w-full gap-y-4">
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
+            <TeamCard />
           </div>
         </div>
       </div>
-    </>
+
+      {/* Our partner */}
+      <div className="bg-white flex flex-col justify-center items-center mt-10 md:mt-[66px] py-10 md:py-[25px] gap-y-5 md:gap-y-[47px]">
+        <div>
+          <Typography
+            className="!text-xl md:!text-2xl font-semibold w-full"
+            fontWeight="bold"
+            align="center"
+          >
+            Partner
+          </Typography>
+          <hr className="w-[58px] h-[2px] bg-[#207BFF] my-2"></hr>
+        </div>
+        <Typography className="!text-base font-normal">
+          It&apos;s an honor to participate with our partner
+        </Typography>
+        <div className="flex justify-center w-full">
+
+        <div className="grid grid-cols-1 md:grid-cols-4 items-center gap-4 md:gap-14">
+          <Link href={"https://sabaicode.com/"}>
+            <Image
+              src={"/assets/image/sabaicode.svg"}
+              alt={"profile"}
+              width={100}
+              height={100}
+              className="object-fill"
+            />
+          </Link>
+          <Link href={"https://chekromlek.com/"}>
+            <Image
+              src={"/assets/image/weshare.png"}
+              alt={"profile"}
+              width={100}
+              height={100}
+            />
+          </Link>
+          <Link href={"https://neakhatka.com/"}>
+            <Image
+              src={"/assets/image/internship.svg"}
+              alt={"profile"}
+              width={100}
+              height={100}
+            />
+          </Link>
+          <Link href={"https://learnwithkru.com/"}>
+            <Image
+              src={"/assets/image/kru.png"}
+              alt={"profile"}
+              width={100}
+              height={100}
+            />
+          </Link>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</>
+
   );
 };
 
--- apps/src/components/molechules/Card/TeamCard.tsx
+++ apps/src/components/molechules/Card/TeamCard.tsx
@@ -5,22 +5,21 @@ import { Typography } from '@/components'
 const TeamCard = () => {
   return (
     <div>
-      <div className='flex items-center flex-col'>
+      {/* box-shadow: 0px 4px 4px 0px #0000001A; */}
+
+      <div className='flex items-center flex-col gap-y-[15px] px-5 py-[25px] shadow-none md:shadow-md rounded-[10px]'>
         <div>
             <Image
               src={"/assets/image/leap.svg"}
               alt={"profile"}
-              width={180}
-              height={180}
-              className="rounded-full border border-[#207bff]"
+              width={100}
+              height={100}
+              className="rounded-full border border-[#207bff]" 
             ></Image>
         </div>
         <div>
-            <Typography align='center'>Peng Maleap</Typography>
-            <Typography align='center'>Former:</Typography>
-            <Typography align='center'>Full-Stack Developer bootcamp 3.0</Typography>
-            <Typography align='center'>Currently:</Typography>
-            <Typography align='center'>UX/UI at SabaiCode</Typography>
+            <Typography className='!text-base' fontWeight='medium' align='center'>Peng Maleap</Typography>
+            <Typography className='!text-base' align='center' fontSize='h3'>Currently : UX/UI Designer</Typography>
         </div>
       </div>
     </div>
