8d2c223f647715c828d136d6adc44ef2159d9c80
Chea_Pinglong <cheapinglong312@gmail.com>
2024-06-05 11:48:48 +0700

    add new page host-notification

---
+++ apps/src/components/molechules/Card/HostNotificationCard.tsx
@@ -0,0 +1,35 @@
+import React, { FC, useState } from "react";
+import Image from "next/image";
+import { Typography } from "@/components/atoms";
+
+interface HostNotificationCardProp {
+  id: string;
+  title: string;
+  image: string;
+  description: string;
+  time: string;
+}
+const HostNotificationCard: FC<HostNotificationCardProp> = ({ title, image, description, time }) => {
+  return (
+    <div>
+      <div className="flex items-center justify-start gap-4 p-3 md:w-[630px] hover:bg-gray-100 rounded-[5px] cursor-pointer">
+        <Image
+          src={`${image}`}
+          alt={"Image"}
+          className="rounded-full"
+          width={68}
+          height={68}
+        />
+        <div className="w-full flex flex-col gap-3">
+          <Typography fontWeight="medium" className="!text-base">
+            {`${title}`} <span className="font-normal text-base">{`${description}`}</span>
+          </Typography>
+          <Typography color="blue">{`${time}`}</Typography>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HostNotificationCard;
