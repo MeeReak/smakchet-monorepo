a442df483ff0f89c147167727a8debab90e9cdee
Mee Reak <138286828+MeeReak@users.noreply.github.com>
2024-06-23 22:42:50 +0700

    deploy fontend

---
+++ apps/src/app/applyForm/layout.tsx
@@ -0,0 +1,39 @@
+import {  Navbar, SecondNarbar } from "@/components";
+import { Metadata } from "next";
+import { Roboto } from "next/font/google";
+import ".././globals.css";
+import { cookies } from "next/headers";
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export const metadata: Metadata = {
+  title: "Smakchet | Apply Form",
+  description: `"SmakChet" is a Website that include all your needed about volunteers. We have process all of your detail about event, date mange users etc.`,
+  // icons: "/icons/logo.svg",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
+  const cookieStore = cookies();
+  const session = cookieStore.get("session");
+  const sigSession = cookieStore.get("session.sig");
+
+  return (
+    <html lang="en">
+      <head>
+        <title>Smakchet</title>
+      </head>
+      <body className={`bg-[#FAFAFA] max-h-full ${roboto.className}`}>
+        <Navbar session={session} sigSession={sigSession} />
+        {children}
+        <SecondNarbar />
+
+        {/* <Footer /> */}
+      </body>
+    </html>
+  );
+}
